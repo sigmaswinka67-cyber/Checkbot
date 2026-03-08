9a9cd09269133d7bb697e5e55032dfc1ba976bf9
sigmaswinka67-cyber <sigmaswinka67@gmail.com>
2026-03-08 19:18:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def dashboard():
 
         last = datetime.fromisoformat(r[2])
 
-        online = datetime.utcnow() - last < timedelta(minutes=10)
+        online = datetime.utcnow() - last < timedelta(minutes=1)
 
         bots.append({
             "id": r[0],
@@ -50,4 +50,5 @@ def dashboard():
 port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=port)
